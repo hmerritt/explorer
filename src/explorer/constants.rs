a22e73f426de5cb2ccdaf3037832a2407e139632
hmerritt <harry@mrrtt.me>
2026-06-02 16:28:39 +0800

    fix: tweak column widths

--- src/explorer/constants.rs
+++ src/explorer/constants.rs
@@ -1,7 +1,7 @@
 pub(super) const COLUMN_NAME_MIN_WIDTH: f32 = 250.0;
 pub(super) const COLUMN_DATE_WIDTH: f32 = 180.0;
-pub(super) const COLUMN_TYPE_WIDTH: f32 = 202.0;
-pub(super) const COLUMN_SIZE_WIDTH: f32 = 124.0;
+pub(super) const COLUMN_TYPE_WIDTH: f32 = 180.0;
+pub(super) const COLUMN_SIZE_WIDTH: f32 = 120.0;
 pub(super) const NAVBAR_HEIGHT: f32 = 52.0;
 pub(super) const NAV_ICON_SIZE_PHYSICAL: f32 = 18.0;
 pub(super) const NAV_ICON_ENABLED_COLOR: u32 = 0x1f1f1f;
